e1568069734a68cfc6be5cef6684ff6a32475fab
huywibuchua02 <huycoijz@gmail.com>
2024-05-15 14:38:08 +0700

    len

--- api.aspx.cs
+++ api.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Data;
+using System.Data.SqlClient;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -11,7 +12,7 @@ namespace BaiTap
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            string connectionString = "Data Source=127.0.0.1,1443;Initial Catalog=chuchua;User Id=sa;Password=123;";
+            string connectionString = "Data Source=127.0.0.1\\SQLEXPRESS;Initial Catalog=chuchua;User Id=sa;Password=123;";
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 using (SqlCommand command = new SqlCommand("SP_Chart", connection))
